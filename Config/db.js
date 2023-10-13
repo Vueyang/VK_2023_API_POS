04250629a7d9c12f1b5e97d6712d4a51a600b747
Vueyang <109055303+Vueyang@users.noreply.github.com>
2023-10-13 10:39:14 +0700

    commit

--- Config/db.js
+++ Config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-const uri = process.env.MONDODB_CONNENT_URI;
-mongoose.Promise = global.Promise
+//require('dotenv').config();
+//const uri = process.env.MONDODB_CONNENT_URI;
+//mongoose.Promise = global.Promise
 //const uri = MONDODB_CONNENT_URI;
 
 const connectDB= async () => {
     try {
-        //await mongoose.connect('mongodb://localhost:27017/test'); //ວີທີນີ້ແມ່ນໃຊ້ເຊື່ມຕໍ່ຈະຖາມຂໍ້ມູນໃນເຄືອງຂອງເຮົາ
+        await mongoose.connect('mongodb://127.0.0.1:27017/product'); //ວີທີນີ້ແມ່ນໃຊ້ເຊື່ມຕໍ່ຈະຖາມຂໍ້ມູນໃນເຄືອງຂອງເຮົາ
 
-        await mongoose.connect(uri);
+        //await mongoose.connect(uri);
        console.log('Successfully connected DB');
     } catch (err) {
         console.log(err);
